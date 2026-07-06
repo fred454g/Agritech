26e71790a8fda34805424bad346d409092cf789d
freddyTheAlch <s220768@dtu.dk>
2026-07-06 18:34:11 +0200

    Moved it to async blinking. Still some work todo, but this should ensure speed

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,25 @@ enum class LedPin : uint8_t {
   Ok = 2
 };
 
+struct Blinker {
+  uint32_t last_toggle_time = 0;
+  bool led_state = false;
+  int blink_counter = 0;
+  int target_blinks = 0;
+  uint32_t interval_ms = 0;
+  LedPin pin;
+
+  // Vi fortæller bare kompilatoren, at disse funktioner findes
+  void start(LedPin led_pin, int n_blinks, uint32_t duration_ms);
+  void update();
+};
+
+
 
 //Declarations
 void setPinMode(LedPin pin, u_int8_t mode);
 void writePin(LedPin pin, u_int8_t value);
-void blink(LedPin pin, int count, uint32_t duration_ms);
-
+Blinker errorBlinker;
 
 void setup()
 {
@@ -24,22 +37,44 @@ void setup()
   writePin(LedPin::Warning, LOW);
   writePin(LedPin::Ok, LOW);
 
+    errorBlinker.start(LedPin::Error, 10, 200);
 }
 
 void loop()
 {
-  blink(LedPin::Error, 10, 200);
+  errorBlinker.update();
 }
 
 
 //functions
 
-void blink(LedPin pin, int count, uint32_t duration_ms){
-  for (int i = 0; i < count; i++){
-    writePin(pin, HIGH);
-    delay(duration_ms); //ms
-    writePin(pin, LOW);
-    delay(duration_ms); //ms
+void Blinker::start(LedPin led_pin, int n_blinks, uint32_t duration_ms) {
+  if (blink_counter == 0) {
+    pin = led_pin;
+    target_blinks = n_blinks * 2;
+    blink_counter = 0;
+    interval_ms = duration_ms;
+    last_toggle_time = millis();
+  }
+}
+
+void Blinker::update() {
+  if (target_blinks == 0) return;
+
+  uint32_t current_time = millis();
+
+  if (current_time - last_toggle_time >= interval_ms) {
+    led_state = !led_state;
+    writePin(pin, led_state ? HIGH : LOW);
+
+    last_toggle_time = current_time;
+    blink_counter++;
+
+    if (blink_counter >= target_blinks) {
+      target_blinks = 0;
+      blink_counter = 0;
+      writePin(pin, LOW);
+    }
   }
 }
 
